f05e1265f382b67ecad9973b9a6d7bb112dc1856
Abdullrhman Aljasser <abdullrhmanaljasser@gmail.com>
2021-11-18 22:59:48 +0300

    refactor: restructured frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {App} from './pages/main';
 import reportWebVitals from './reportWebVitals';
+import './styling/main.scss'
 
 ReactDOM.render(
   <React.StrictMode>
