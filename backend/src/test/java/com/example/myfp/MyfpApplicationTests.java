5459788c6ea20c7e2e17cfc9501243bb39e4c064
Abdullrhman Aljasser <abdullrhmanaljasser@gmail.com>
2021-11-18 16:43:28 +0300

    feature: initialized project infeststructure

---
+++ backend/src/test/java/com/example/myfp/MyfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
